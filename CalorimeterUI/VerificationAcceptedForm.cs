d998a1c7ff71cc4c38a993819d1c4b3268cc0694
omerfdev <omerfdev@gmail.com>
2023-07-12 14:39:41 +0300

    Commit

--- CalorimeterUI/VerificationAcceptedForm.cs
+++ CalorimeterUI/VerificationAcceptedForm.cs
@@ -16,5 +16,28 @@ namespace CalorimeterUI
 		{
 			InitializeComponent();
 		}
+
+		private void VerificationAcceptedForm_Load(object sender, EventArgs e)
+		{
+			if (SignIn.DarkMode)
+			{
+				lblVerified.ForeColor = Color.DarkKhaki;
+				lblVerifiedInfo.ForeColor = Color.DarkKhaki;
+				this.BackgroundImage = Image.FromFile("..\\..\\..\\Resources\\darkbackground.png");
+			}
+			else if (SignIn.DarkMode == false)
+			{
+				lblVerified.ForeColor = Color.Olive;
+				lblVerifiedInfo.ForeColor = Color.Olive;
+				this.BackgroundImage = Image.FromFile("..\\..\\..\\Resources\\background.png");
+			}
+			this.Size = new Size(420, 450);
+			this.Location = this.Owner.Location;
+		}
+
+		private void btnOk_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
 	}
 }
