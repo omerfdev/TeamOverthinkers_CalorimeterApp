d96d4fd163c4e045982db30c265655fecf784c0e
omerfdev <omerfdev@gmail.com>
2023-07-12 14:45:56 +0300

    Commit

--- CalorimeterUI/VerificationAcceptedForm.Designer.cs
+++ CalorimeterUI/VerificationAcceptedForm.Designer.cs
@@ -28,85 +28,89 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			btnSignIn = new DrakeUI.Framework.DrakeUIButton();
-			label2 = new Label();
-			label1 = new Label();
-			pictureBox1 = new PictureBox();
-			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VerificationAcceptedForm));
+			btnOk = new DrakeUI.Framework.DrakeUIButton();
+			lblVerifiedInfo = new Label();
+			lblVerified = new Label();
+			pbVerified = new PictureBox();
+			((System.ComponentModel.ISupportInitialize)pbVerified).BeginInit();
 			SuspendLayout();
 			// 
-			// btnSignIn
+			// btnOk
 			// 
-			btnSignIn.FillColor = Color.DarkOliveGreen;
-			btnSignIn.FillHoverColor = Color.DarkKhaki;
-			btnSignIn.FillPressColor = Color.DarkKhaki;
-			btnSignIn.FillSelectedColor = Color.DarkKhaki;
-			btnSignIn.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point);
-			btnSignIn.ForeColor = Color.Khaki;
-			btnSignIn.Location = new Point(127, 288);
-			btnSignIn.Margin = new Padding(3, 4, 3, 4);
-			btnSignIn.Name = "btnSignIn";
-			btnSignIn.RectColor = Color.DarkKhaki;
-			btnSignIn.RectDisableColor = Color.FromArgb(227, 242, 253);
-			btnSignIn.RectHoverColor = Color.Olive;
-			btnSignIn.Size = new Size(95, 31);
-			btnSignIn.Style = DrakeUI.Framework.UIStyle.Custom;
-			btnSignIn.TabIndex = 8;
-			btnSignIn.Text = "OK";
+			btnOk.FillColor = Color.DarkOliveGreen;
+			btnOk.FillHoverColor = Color.DarkKhaki;
+			btnOk.FillPressColor = Color.DarkKhaki;
+			btnOk.FillSelectedColor = Color.DarkKhaki;
+			btnOk.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point);
+			btnOk.ForeColor = Color.Khaki;
+			btnOk.Location = new Point(151, 295);
+			btnOk.Margin = new Padding(3, 4, 3, 4);
+			btnOk.Name = "btnOk";
+			btnOk.RectColor = Color.DarkKhaki;
+			btnOk.RectDisableColor = Color.FromArgb(227, 242, 253);
+			btnOk.RectHoverColor = Color.Olive;
+			btnOk.Size = new Size(95, 31);
+			btnOk.Style = DrakeUI.Framework.UIStyle.Custom;
+			btnOk.TabIndex = 8;
+			btnOk.Text = "OK";
 			// 
-			// label2
+			// lblVerifiedInfo
 			// 
-			label2.AutoSize = true;
-			label2.BackColor = Color.Transparent;
-			label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-			label2.Location = new Point(46, 252);
-			label2.Name = "label2";
-			label2.Size = new Size(294, 20);
-			label2.TabIndex = 7;
-			label2.Text = "You have successfully verified your account.";
+			lblVerifiedInfo.AutoSize = true;
+			lblVerifiedInfo.BackColor = Color.Transparent;
+			lblVerifiedInfo.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+			lblVerifiedInfo.Location = new Point(70, 259);
+			lblVerifiedInfo.Name = "lblVerifiedInfo";
+			lblVerifiedInfo.Size = new Size(294, 20);
+			lblVerifiedInfo.TabIndex = 7;
+			lblVerifiedInfo.Text = "You have successfully verified your account.";
 			// 
-			// label1
+			// lblVerified
 			// 
-			label1.AutoSize = true;
-			label1.BackColor = Color.Transparent;
-			label1.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
-			label1.Location = new Point(115, 192);
-			label1.Name = "label1";
-			label1.Size = new Size(115, 35);
-			label1.TabIndex = 6;
-			label1.Text = "Verified!";
+			lblVerified.AutoSize = true;
+			lblVerified.BackColor = Color.Transparent;
+			lblVerified.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+			lblVerified.Location = new Point(139, 199);
+			lblVerified.Name = "lblVerified";
+			lblVerified.Size = new Size(115, 35);
+			lblVerified.TabIndex = 6;
+			lblVerified.Text = "Verified!";
 			// 
-			// pictureBox1
+			// pbVerified
 			// 
-			pictureBox1.BackColor = Color.Transparent;
-			pictureBox1.Location = new Point(88, 27);
-			pictureBox1.Name = "pictureBox1";
-			pictureBox1.Size = new Size(159, 163);
-			pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-			pictureBox1.TabIndex = 5;
-			pictureBox1.TabStop = false;
+			pbVerified.BackColor = Color.Transparent;
+			pbVerified.Image = (Image)resources.GetObject("pbVerified.Image");
+			pbVerified.Location = new Point(112, 34);
+			pbVerified.Name = "pbVerified";
+			pbVerified.Size = new Size(159, 163);
+			pbVerified.SizeMode = PictureBoxSizeMode.StretchImage;
+			pbVerified.TabIndex = 5;
+			pbVerified.TabStop = false;
 			// 
 			// VerificationAcceptedForm
 			// 
 			AutoScaleDimensions = new SizeF(8F, 20F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(800, 450);
-			Controls.Add(btnSignIn);
-			Controls.Add(label2);
-			Controls.Add(label1);
-			Controls.Add(pictureBox1);
+			BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+			ClientSize = new Size(409, 379);
+			Controls.Add(btnOk);
+			Controls.Add(lblVerifiedInfo);
+			Controls.Add(lblVerified);
+			Controls.Add(pbVerified);
+			FormBorderStyle = FormBorderStyle.None;
 			Name = "VerificationAcceptedForm";
 			Text = "VerificationAcceptedForm";
-			((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+			((System.ComponentModel.ISupportInitialize)pbVerified).EndInit();
 			ResumeLayout(false);
 			PerformLayout();
 		}
 
 		#endregion
 
-		private DrakeUI.Framework.DrakeUIButton btnSignIn;
-		private Label label2;
-		private Label label1;
-		private PictureBox pictureBox1;
+		private DrakeUI.Framework.DrakeUIButton btnOk;
+		private Label lblVerifiedInfo;
+		private Label lblVerified;
+		private PictureBox pbVerified;
 	}
 }
